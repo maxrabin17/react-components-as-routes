117b39032fc7c99a2cf1a13ea51296d6afaef4fa
maxrabin <max.rabin17@gmail.com>
2021-06-04 10:42:56 -0400

    Completed Assignment

---
+++ src/Messages.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Messages extends React.Component {
+    render() {
+        return <h1>This is my Messages component!</h1>;
+    }
+}
+
+export default Messages;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import Signup from './Signup'
+import Messages from './Messages'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-const Home = () => {
-  return (
+
+
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <NavLink
+//       to="/" 
+//       exact 
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}>
+//       Home
+//       </NavLink>
+//       <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}>
+//       About
+//       </NavLink>
+//       <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}>
+//       Login
+//       </NavLink>
+//       <NavLink
+//       to="/signup"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}>
+//       Signup  
+//       </NavLink>
+//       <NavLink
+//       to="/messages"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}>
+//       Messages 
+//       </NavLink>
+//     </div>
+//   )
+// }
+
+// const Home = () => <h1>Home!</h1>;
+
+// const Signup = () => <h1>Signup</h1>;
+
+// const Messages = () => <h1>Messages</h1>;
+
+// const About = () => <h1>This is my about component!</h1>;
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username"/>
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password"/>
+//           <label htmlFor="password">Password</label>
+//         </div>  
+//         <input type="submit" value="Login"/>
+//       </form>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+ReactDOM.render((
+  <Router> 
     <div>
-      <h1>Home!</h1>
+      <Navbar />  
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+      <Route exact path="/messages" component={Messages}/>
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
